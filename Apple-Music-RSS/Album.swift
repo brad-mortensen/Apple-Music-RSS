09f9fb351dce3f4b354719cb309c14e43f5daddd
Brad Mortensen <b.mort86@gmail.com>
2020-02-09 21:20:48 -0600

    adds data stuff

---
+++ Apple-Music-RSS/Album.swift
@@ -0,0 +1,24 @@
+//
+//  Album.swift
+//  Apple-Music-RSS
+//
+//  Created by Bradley Mortensen on 2/9/20.
+//  Copyright © 2020 Brad Mortensen. All rights reserved.
+//
+
+import Foundation
+
+struct AlbumResults:Decodable {
+    var results:Albums
+}
+
+struct Albums:Decodable {
+    var albums:[AlbumDetails]
+}
+
+struct AlbumDetails:Decodable {
+    var artistName:String
+    var name:String
+    var artworkUrl100:String
+}
+
